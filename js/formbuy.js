19aa608346d325c17fd2cd06d9229aa17eaee3f4
Alexaandrov <alexaandrov@gmail.com>
2015-12-05 21:47:19 +0300

    added post method to forms

--- js/formbuy.js
+++ js/formbuy.js
@@ -1,36 +1,69 @@
-var formBuy = document.getElementById('form-buy');
-var anchors = document.getElementById('anchors');
+	var formBuy = document.getElementById('form-buy');
+	var anchors = document.getElementById('anchors');
+	document.getElementById('buy').onclick = function() {
+		formBuy.style.cssText="display: block;";
+		anchors.style.cssText="display: none";
+	}
+	document.getElementById('quit').onclick = function() {
+		formBuy.style.cssText="display: none";
+		anchors.style.cssText="display: block";
+	}
 
-document.getElementById('buy').onclick = function() {
-	formBuy.style.cssText="display: block;";
-	anchors.style.cssText="display: none";
-};
-
-document.getElementById('quit').onclick = function() {
-	formBuy.style.cssText="display: none";
-	anchors.style.cssText="display: block";
-}
-buyers = 1;
-p = document.getElementById('req-number');
-p.innerHTML = buyers;
-function addBuyer() {
-	p.innerHTML = ++buyers;
-}
-
-function checkIssues() {
-	var issues = document.forms.issues;
-
-	document.getElementById('issue').onclick = function() {
-		if (issues.nick.value == "") {
+$(function () {
+	var buyers = 1;
+	var p = document.getElementById('req-number');
+	p.innerHTML = buyers;
+	function addBuyer() {
+		p.innerHTML = ++buyers;
+	}
+	function checkIssues(name, phone) {
+		if (name == "") {
 			alert("Напишите ваше имя");
 			return;
 		} 
-		if (issues.phone.value == "") {
+		if (phone == "") {
 			alert("Напишите ваш телефон");
 			return;
 		}
-		addBuyer();
-		alert("Спасибо! Ваша заявка принята!")
-		formBuy.style.cssText="display: none";
+		if (phone / 1 > 1 === false) {
+			alert("Номер телефона введен не корректно");
+			return;
+		}
+		if (phone.length < 10)
+		{
+			alert("Телефон не должен быть < 10 цифр");
+			return;
+		}
+		return true;
 	}
-};
+
+	var btn = $('.sumbit-form-buy');
+		btn.click(function(e) {
+		e.preventDefault();
+		var name = $('.name').val();
+		var phone = $('.phone').val();
+		if(checkIssues(name, phone))
+		{
+			$.ajax({
+				'url': 'http://requests.xyzslash.ru/form_data/',
+				'type': 'POST',
+				'data': {
+				  'name': name,
+				  'phone': phone
+				},
+				success: function() {
+					addBuyer();
+					alert("Спасибо! Ваша заявка принята!")
+					formBuy.style.cssText="display: none";
+					$('.name').val('');
+					$('.phone').val('');
+				},
+				error: function() {
+					alert("Ошибка при отправке данных");
+					
+				}
+			});
+		}
+	});
+
+});
---
+++ js/formsend.js
@@ -0,0 +1,47 @@
+$(function() {
+	function checkIssues(login, email, text) {
+		if (login == "") {
+			alert("Напишите ваше имя");
+			return;
+		} 
+		if (email == "") {
+			alert("Напишите ваш email");
+			return;
+		}
+		var emailValid = /^\w+@\w+\.\w{2,4}$/i;
+		if (!emailValid.test(email)) {
+			alert("Email введен некорректно");
+			return;
+		}
+		return true;
+	}
+
+	var btn = $('.send-form');
+	btn.click(function(e) {
+		e.preventDefault();
+		
+		var login = $('.login').val();
+		var email = $('.email').val();
+		var text = $('.textare').val();
+		if(checkIssues(login, email, text)) {
+			$.ajax({
+				'url': 'http://requests.xyzslash.ru/form_data/',
+				'type': 'POST',
+				'data': {
+				  'login': login,
+				  'emai': email,
+				  'text': text
+				},
+				success: function() {
+					addBuyer();
+					alert("Спасибо! Ваша заявка принята!")
+					formBuy.style.cssText="display: none";
+				},
+				error: function() {
+					alert("Ошибка при отправке данных");
+					login="";
+				}
+			});
+		}
+	});
+});
